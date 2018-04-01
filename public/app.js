40a91f36e32e72653e64cffa8b9bddd4d77e4d50
Christopher Johnson <cjhawk7@hotmail.com>
2018-04-01 12:36:22 -0700

    query search input issue

--- public/app.js
+++ public/app.js
@@ -15,23 +15,28 @@
 
 
 function getNumbeoData(searchTerm, callback) {
+    console.log(searchTerm);
     const settings = {
         url: `https://www.numbeo.com/api/city_prices?api_key=4uxocu7eiqwid6&query=${searchTerm}`,
         dataType: 'json',
         type: 'GET',
         success: callback,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE'
-        }
+        // headers: {
+        //     'Access-Control-Allow-Origin': '*',
+        //     'Access-Control-Allow-Headers': 'Content-Type',
+        //     'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,DELETE'
+        // }
     };
     $.ajax(settings);  
 }
 
+// function numberOfMeals() {
+//     let queryBudget = $()
+//     $('.container-results').append() 
+// }
+
 
 function displayNumbeoData(data) {
-
     $('.container-results').append(data.prices[0].average_price);
 }
 
@@ -39,8 +44,9 @@ function displayNumbeoData(data) {
 function getAndDisplayNumbeoData() {
     $('.search').submit(event => {
         event.preventDefault();
-        const queryTarget = $(event.currentTarget).find('.js-query-name');
+        const queryTarget = $(event.currentTarget).find('.js-query-search'[1]);
         const query = queryTarget.val();
+        console.log('hi', queryTarget.val());
         getNumbeoData(query, displayNumbeoData);
     });
 };
