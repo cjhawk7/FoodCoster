bc175ac46baca5093a67818fd66d6ab710450316
Christopher Johnson <cjhawk7@hotmail.com>
2018-08-07 12:27:03 -0700

    fixed footer on mobile

--- public/app.js
+++ public/app.js
@@ -411,6 +411,22 @@ function displayComparison(obj) {
   }
 }
 
+function footerAlign() {
+  $(".footer").css("display", "block");
+  $(".footer").css("height", "auto");
+  var footerHeight = $(".footer").outerHeight();
+  $("body").css("padding-bottom", footerHeight);
+  $(".footer").css("height", footerHeight);
+}
+
+$(document).ready(function() {
+  footerAlign();
+});
+
+$(window).resize(function() {
+  footerAlign();
+});
+
 function setupClickHandlers() {
   $(".search").submit(event => {
     event.preventDefault();
